3b53e09cfde0d6e3a442e97258dd6b58008c83cd
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-10 17:41:26 +0900

    feat: whatgpt3 html

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import './whatGPT3.scss'
+import React from "react";
+import { Feature } from "../../components";
+import "./whatGPT3.scss";
 
 const WhatGPT3 = () => {
   return (
-    <div className='hello'>WhatGPT3
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-header">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
