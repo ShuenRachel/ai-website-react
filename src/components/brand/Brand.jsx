943e550b3abad57ba1b1b5c06ac81d72fc4bca8f
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-10 17:16:32 +0900

    feat: import image to brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import './brand.scss'
+import React from "react";
+import "./brand.scss";
+import {
+  googleImg,
+  slackImg,
+  atlassianImg,
+  dropboxImg,
+  shopifyImg,
+} from "./import";
+
+const images = [googleImg, slackImg, atlassianImg, dropboxImg, shopifyImg];
+
+const imageItems = images.map((image) => (<div key={image}><img src={image} alt=''/></div>))
 
 const Brand = () => {
-  return (
-    <div className='hello'>Brand
-    </div>
-  )
-}
+  return <div className="gpt3__brand section__padding">{imageItems}</div>;
+};
 
-export default Brand
+export default Brand;
