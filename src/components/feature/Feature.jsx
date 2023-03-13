f3a2c1abce353130278b87b942f2898b010b4199
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-13 17:17:03 +0900

    feat: featur box

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import './feature.scss'
+import React from "react";
+import "./feature.scss";
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div className='hello'>Feature
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div className="gpt3__features-container__feature-title__line" />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
